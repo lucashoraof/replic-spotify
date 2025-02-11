c5d8832e97c1934b809942eba5348b6beba613dc
lucashoraof <lucashorarj@gmail.com>
2025-02-11 15:22:15 -0300

    feat: creanting content main

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="main__texts">
+        <h2>Artitas populares</h2>
+        <a className="main__link" href="/"> Mostrar tudo</a>
+      </div>
+    </div>
+  )
+}
+
+export default Main
